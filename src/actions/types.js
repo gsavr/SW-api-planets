6e72d9f5ae4b14a759a13c1725dfb0474a06c047
giorgio <giorgiosavron@me.com>
2022-08-01 19:50:38 -0400

    updated redux

--- src/actions/types.js
+++
@@ -1,4 +0,0 @@
-export const FETCH_PLANETS = "fetch_planets";
-export const FETCH_SINGLE_PLANET = "fetch_singleplanet";
-export const FETCH_FILM = "fetch_film";
-export const FETCH_RESIDENT = "fetch_resident";
